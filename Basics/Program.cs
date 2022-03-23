de49363909d13c14623eea291eca90ccb6621004
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-03-23 15:37:28 -0400

    Enum created

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -8,6 +8,13 @@ using System.Threading.Tasks;
 
 namespace Basics
 {
+    public enum ShippingMethod 
+    {
+        RegularAirMail = 1,
+        RegisteredAirMail = 2,
+        Express = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
