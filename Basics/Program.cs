0619dc7c929d156611a5787be26afc8e8a279f09
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-03-23 12:52:17 -0400

    Array of numbers

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,4 +1,5 @@
-﻿using Basics.Math;
+﻿// Import in Calculator method
+using Basics.Math;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,18 +12,34 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            // Hello World
             string message = "Hello World!! This is my first C# Application";
-
             Console.WriteLine(message);
 
+            // Bring in Person method from Person.cs
             var john = new Person();
             john.FirstName = "John";
             john.LastName = " Smith";
             john.Introduce();
 
+            // Calculator Method from Basics.Math
             Calculator calculator = new Calculator();
             var result = calculator.Add(1, 2);
             Console.WriteLine(result);
+
+            // In C#, Arrays are fixed in size
+            // Specify during declaration and unable to change
+            // Can also declare what is part of the Array in declaration
+            var numbers = new int[3];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+
         }
     }
 }
