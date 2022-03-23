4a95ac60245072ffa469cf73d8824ceda43d2bb6
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-03-23 12:53:34 -0400

    Boolean Array

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -39,6 +39,16 @@ namespace Basics
             Console.WriteLine(numbers[1]);
             Console.WriteLine(numbers[2]);
 
+            // Boolean Array
+            var flags = new bool[3];
+
+            flags[0] = true;
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+
+
 
         }
     }
