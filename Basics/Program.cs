37c25b8a85a7eb14be8d172dabaf64da016313de
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-03-24 15:36:47 -0400

    array reference types. arr2 overrites arr1

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -86,6 +86,12 @@ namespace Basics
             b++;
             Console.WriteLine(String.Format("a: {0}, b: {1}", a, b));
 
+            // Reference Types- Arrays
+            var array1 = new int[3] { 1, 2, 3 };
+            var array2 = array1;
+            array2[0] = 0;
+            Console.WriteLine(String.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+
         }
     }
 }
